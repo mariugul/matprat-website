f19f48e6faf81431bc847d3856f4fd273c12c4d6
mgulbrandsen <marius.gulbrandsen@zenitel.com>
2021-11-20 23:16:53 +0100

    Update sql insert files to work when called

--- database/sql/insert_ingredients.sql
+++ database/sql/insert_ingredients.sql
@@ -1,11 +1,5 @@
-INSERT INTO ingredients (recipe_name, name, amount, unit)
+INSERT INTO ingredients (recipe_name, ingredient, amount, unit)
 VALUES 
 ('Waffles', 'Eggs', 4, 'large'),
 ('Waffles', 'Milk', 4, 'dl'),
 ('Waffles', 'Flour', 4, 'dl');
-
-SELECT 'q1' AS source, a, b, c, d FROM t1 WHERE ...
-UNION ALL
-SELECT 'q2', t2.fee, t2.fi, t2.fo, 'fum' FROM t2 JOIN t3 ON ...
-UNION ALL
-SELECT 'q3', '1', '2', buckle, my_shoe FROM t4
