7683b7e066bc799ff0d80e2f8b9b96bf4618a28b
mariugul <marius_gulbrandsen@hotmail.no>
2021-12-08 23:55:32 +0000

    Add sqlfluff linter to pipeline

--- database/lint.sh
+++ database/lint.sh
@@ -2,4 +2,4 @@
 #
 # This script lints all of the SQL files with the tool sqlfluff using the
 # configurations defined in .sqlfluff.
-sqlfluff lint --ignore parsing
+sqlfluff lint --ignore parsing -v 
