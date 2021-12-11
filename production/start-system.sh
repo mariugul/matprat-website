99adabc364fc68846a14911fbadc8f5e1d753e3b
mariugul <marius_gulbrandsen@hotmail.no>
2021-12-11 19:12:23 +0000

    Add "detached" to docker compose up

--- production/start-system.sh
+++ production/start-system.sh
@@ -2,7 +2,7 @@
 
 # Start the system with docker-compose up
 # This will also update the images if there are updates
-docker-compose -f docker-compose.yml -f webhooks/docker-compose.yml up --remove-orphan 
+docker-compose -f docker-compose.yml -f webhooks/docker-compose.yml up --remove-orphan -d
 
 # Start webhooks
 # webhook -hooks webhooks/hooks.json -verbose -hotreload
