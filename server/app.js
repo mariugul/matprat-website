952863afa1ec4c9b1f176e6c8354fb2c1570e6dd
mariugul <marius_gulbrandsen@hotmail.no>
2021-12-13 01:21:48 +0000

    Fix lint errors in app.js

--- server/app.js
+++ server/app.js
@@ -1,40 +1,40 @@
-const { render } = require("ejs");
-const express = require("express");
-const { object } = require("joi");
+/* eslint-disable no-console */
+/* eslint-disable no-return-assign */
+const express = require('express');
 const process = require('process');
 // var cors = require('cors') // CORS for local development
 
-var app = express();
+const app = express();
 
-app.set("view engine", "ejs"); // use EJS as template engine
-app.use(express.static("public"));
-app.use(express.static("public/css"));
-app.use(express.static("public/js"));
-app.use(express.static("public/images"));
+app.set('view engine', 'ejs'); // use EJS as template engine
+app.use(express.static('public'));
+app.use(express.static('public/css'));
+app.use(express.static('public/js'));
+app.use(express.static('public/images'));
 // app.use(express.json(), cors()); // Allow CORS
 
 //
-process.on('SIGINT', function onSigint() {
+process.on('SIGINT', () => {
   app.shutdown();
 });
 
-process.on('SIGTERM', function onSigterm() {
+process.on('SIGTERM', () => {
   app.shutdown();
 });
 
-app.shutdown = function () {
-  // clean up your resources and exit 
+app.shutdown = () => {
+  // clean up your resources and exit
   process.exit();
 };
 
-
 // Set up postgres parameters
-const { Pool, Client } = require("pg");
+const { Pool } = require('pg');
+
 const pool = new Pool({
-  user: process.env.DB_USER || "nodejs",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB || "matprat",
-  password: process.env.DB_PASSWORD || "nodejs",
+  user: process.env.DB_USER || 'nodejs',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB || 'matprat',
+  password: process.env.DB_PASSWORD || 'nodejs',
   port: process.env.DB_PORT || 5432,
 });
 
@@ -42,6 +42,7 @@ const pool = new Pool({
 // ----------------------------------------------
 async function sqlQuery(query, queryArgs) {
   return new Promise((resolve, reject) => {
+    // eslint-disable-next-line consistent-return
     pool.query(query, queryArgs, (err, res) => {
       if (err) return reject(err);
       // console.log(res.rows);
@@ -54,33 +55,35 @@ async function sqlQuery(query, queryArgs) {
 //-------------------------------------------
 
 // Home page
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 // Recipes
-app.get("/recipes", (req, res) => {
-  res.render("recipes");
+app.get('/recipes', (req, res) => {
+  res.render('recipes');
 });
 
 // Display a specific recipe
-app.get("/recipes/:name", (req, res) => {
+app.get('/recipes/:name', (req, res) => {
   // There are 4 queries to perform and it matters that the recipeInfo checks
   // whether the recipe exists first and then the following 3 queries need to finish
   // before the page can be generated and sent back. Therfore an inline async function
   // is declared here to use the 'await' keyword on the sqlQuery function, which is also async.
-  queries = (async function () {
-    var recipeExists = false;
+  // eslint-disable-next-line no-undef
+  queries = (async function queryDatabase() {
+    let recipeExists = false;
+    let recipeInfo;
 
     // Get info on recipe. If it doesn't exist, display error page
-    await sqlQuery("SELECT * FROM recipeInfo($1)", [req.params.name])
+    await sqlQuery('SELECT * FROM recipeInfo($1)', [req.params.name])
       .then((result) => {
-        if (result === undefined || result.length == 0) {
-          res.render("error", {
+        if (result === undefined || result.length === 0) {
+          res.render('error', {
             errorMessage:
-              'The recipe "' +
-              req.params.name +
-              '" does not exist in the database.',
+              `The recipe "${
+                req.params.name
+              }" does not exist in the database.`,
           });
           recipeExists = false;
         } else {
@@ -88,80 +91,83 @@ app.get("/recipes/:name", (req, res) => {
           recipeInfo = result;
         }
       })
-      .catch((err) =>
-        res.render("error", {
-          errorMessage: err,
-        })
-      );
+      .catch((err) => res.render('error', {
+        errorMessage: err,
+      }));
 
     // If no recipe was returned from the previous query, exit this function
     if (!recipeExists) {
       return 0;
     }
 
+    // Define vars for query results
+    let ingredients;
+    let steps;
+    let images;
+
     // Get recipe ingredients
-    await sqlQuery("SELECT * FROM ingredients($1)", [req.params.name])
+    await sqlQuery('SELECT * FROM ingredients($1)', [req.params.name])
       .then((result) => (ingredients = result))
       .catch((err) => console.log(err));
 
     // Get recipe steps (instructions)
-    await sqlQuery("SELECT * FROM steps($1)", [req.params.name])
+    await sqlQuery('SELECT * FROM steps($1)', [req.params.name])
       .then((result) => (steps = result))
       .catch((err) => console.log(err));
 
     // Get recipe images (links)
-    await sqlQuery("SELECT * FROM images($1)", [req.params.name])
+    await sqlQuery('SELECT * FROM images($1)', [req.params.name])
       .then((result) => (images = result))
       .catch((err) => console.log(err));
 
     // Generate the webpage from template and send back
-    res.render("recipe", {
-      recipeInfo: recipeInfo,
-      ingredients: ingredients,
-      steps: steps,
-      images: images,
+    res.render('recipe', {
+      recipeInfo,
+      ingredients,
+      steps,
+      images,
     });
-  })();
+
+    return 0;
+  }());
 });
 
 // POSTGRES ----------------------------------------
 // Get recipe information by name
-app.get("/api/db/select/recipe/:name", (req, res) => {
+app.get('/api/db/select/recipe/:name', (req, res) => {
   // Get recipe information by name
   pool.query(
-    "SELECT * FROM recipes WHERE name=$1",
+    'SELECT * FROM recipes WHERE name=$1',
     [req.params.name],
-    (err, res_db) => {
+    (err, resDb) => {
       if (err) {
         // console.log(err.stack);
         return res
           .status(404)
-          .send("There was an error getting the recipes from the database.");
-      } else {
-        // console.table(res_db.rows[0]);
-        return res.status(200).send(res_db.rows[0]);
+          .send('There was an error getting the recipes from the database.');
       }
-    }
+      // console.table(res_db.rows[0]);
+      return res.status(200).send(resDb.rows[0]);
+    },
   );
 });
 
 // Get all recipe names
-app.get("/api/db/select/recipes", (req, res) => {
+app.get('/api/db/select/recipes', (req, res) => {
   const query = {
-    text: "SELECT name FROM recipes",
-    rowMode: "array",
+    text: 'SELECT name FROM recipes',
+    rowMode: 'array',
   };
 
-  pool.query(query, (err, res_db) => {
+  pool.query(query, (err, resDb) => {
     if (err) {
       // console.log(err.stack);
       return res
         .status(404)
-        .send("There was an error getting the recipe names from the database.");
-    } else {
-      // console.table(res_db.rows);
-      return res.status(200).send(res_db.rows);
+        .send('There was an error getting the recipe names from the database.');
     }
+    // console.table(res_db.rows);
+    return res.status(200).send(resDb.rows);
   });
 });
 
