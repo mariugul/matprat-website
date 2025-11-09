f15a6bc606e0bc9b3ba76b38b156544f08e3cada
Marius C. K. Gulbrandsen <marius_gulbrandsen@hotmail.no>
2025-11-09 22:53:24 +0100

    feat: add Docker-based SQL linting option
    - Created lint-docker.sh script to run SQLFluff in an isolated Docker container for consistent linting across environments
    - Updated lint.sh to explicitly specify postgres dialect for more accurate linting

--- database/lint.sh
+++ database/lint.sh
@@ -2,4 +2,4 @@
 #
 # This script lints all of the SQL files with the tool sqlfluff using the
 # configurations defined in .sqlfluff.
-sqlfluff lint --ignore parsing -v 
+sqlfluff lint --dialect postgres --ignore parsing -v
